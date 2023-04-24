8beb8d3d37417b000fb10ce2abe4db7df8d8c4c2
XuanTam Hoang <xuantam.hoang23@gmail.com>
2023-04-24 15:20:02 +0200

    Changed fetch syntax (.then -> async/await) and added event listener to filter btn

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,50 +1,63 @@
 const galleryDiv = document.querySelector(".gallery");
 
 // Access to API /works
-fetch('http://localhost:5678/api/works')
-    .then(reponse => reponse.json())
-    .then(outputData => {
+async function fetchWorks() {
+    const r = await fetch("http://localhost:5678/api/works")
+    if (r.ok === true){
+        return r.json();
+    }
+    throw new Error("Impossible de contacter le serveur")
+}
 
-        console.table(outputData)
+fetchWorks().then(works => {
 
-        // Loop the array and create <figure> for each element
-        for (let i = 0; i < outputData.length; i++){
-            const projectFigure = document.createElement("figure")
-            const projectImg = document.createElement("img")
+    // Loop the array and create <figure> for each element
+    for (let i = 0; i < works.length; i++){
+        const projectFigure = document.createElement("figure")
+        const projectImg = document.createElement("img")
 
-            // Create <img> element and fill the attributes src, alt
-            projectImg.src = outputData[i].imageUrl
-            projectImg.alt = outputData[i].title
-            projectFigure.appendChild(projectImg)
+        // Create <img> element and fill the attributes src, alt
+        projectImg.src = works[i].imageUrl
+        projectImg.alt = works[i].title
+        projectFigure.appendChild(projectImg)
 
-            // Create <figcaption> element and fill the element
-            const projectName = document.createElement("figcaption")
-            projectName.innerText = outputData[i].title
-            projectFigure.appendChild(projectName)
+        // Create <figcaption> element and fill the element
+        const projectName = document.createElement("figcaption")
+        projectName.innerText = works[i].title
+        projectFigure.appendChild(projectName)
 
-            // Create <figure> child element into the gallery section
-            galleryDiv.appendChild(projectFigure)
-        }
+        // Create <figure> child element into the gallery section
+        galleryDiv.appendChild(projectFigure)
+    }
 
-    })
+    const filteredWorks = works.filter(work => work.categoryId > 2)
+})
 
 // Access to API /categories
-fetch('http://localhost:5678/api/categories')
-    .then(reponse => reponse.json())
-    .then(outputCategData =>{
+async function fetchCateg() {
+    const r = await fetch("http://localhost:5678/api/categories")
+    if (r.ok === true){
+        return r.json();
+    }
+    throw new Error("Impossible de contacter le serveur")
+}
 
-        const filtersList = document.querySelector(".filters-list")
-        console.table(outputCategData)
+fetchCateg().then(categories => {
 
-        for (const category of outputCategData){
-            let filterBtn = document.createElement("li")
-            filterBtn.classList.add("filter-btn")
-            filterBtn.innerText = category.name
-            filtersList.appendChild(filterBtn)
-        }
-        /*
-        const filters = document.querySelector(".filter-btn")
-        filters.addEventListener("click", function() {
-            console.log("Hey")
-        })*/
+    const filtersList = document.querySelector(".filters-list")
+
+    for (const category of categories){
+        let filterBtn = document.createElement("li")
+        filterBtn.classList.add("filter-btn")
+        filterBtn.innerText = category.name
+        filtersList.appendChild(filterBtn)
+    }
+})
+
+
+const filters = document.querySelectorAll(".filter-btn")
+filters.forEach(function(){
+    this.addEventListener("click", () =>{
+        console.log("Hey")
     })
+})
